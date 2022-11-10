10c71a1b864c8a88eccfe19830d6aa3208f12e5a
alexandr <Alexandr6732@mail.ru>
2022-11-10 12:53:09 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// const obj = {
+//     a:30
+// }
+
+// obj.a=50;
+
+// console.log(obj);
+
+// посмотрим на работу prompt и сохраним ответы в массив
+
+const answers = [];
+
+answers[0]=prompt('Как вас зовут?','');
+answers[1]=prompt('Сколько вам лет?','');
+answers[2]=prompt('Как жызнь?','');
+
+
+console.log(typeof(answers));
